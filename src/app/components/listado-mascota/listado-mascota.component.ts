d48f27cfc658fe2b594bd06ac580c332750b7ef0
Joaquin Acosta <joaquinnof14@gmail.com>
2024-01-15 16:32:22 -0300

    agregado ordenamiento y filtro

--- src/app/components/listado-mascota/listado-mascota.component.ts
+++ src/app/components/listado-mascota/listado-mascota.component.ts
@@ -1,5 +1,6 @@
 import { AfterViewInit, Component, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { Mascota } from 'src/app/interfaces/mascota';
 
@@ -22,10 +23,17 @@ export class ListadoMascotaComponent implements AfterViewInit {
   dataSource = new MatTableDataSource<Mascota>(listMascotas);
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
 
   ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
     this.paginator._intl.itemsPerPageLabel = 'Items por página'
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
 }
