d8d34048eb16d120e7fadcebf5249d381f8aaf33
Joaquin Acosta <joaquinnof14@gmail.com>
2024-01-15 21:55:28 -0300

    agregado mensaje de exito de mascota eliminada y spinner

--- src/app/components/listado-mascota/listado-mascota.component.ts
+++ src/app/components/listado-mascota/listado-mascota.component.ts
@@ -1,5 +1,6 @@
 import { AfterViewInit, Component, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { Mascota } from 'src/app/interfaces/mascota';
@@ -21,9 +22,12 @@ const listMascotas: Mascota[] = [
 export class ListadoMascotaComponent implements AfterViewInit {
   displayedColumns: string[] = ['nombre', 'edad', 'raza', 'color', 'peso', 'acciones'];
   dataSource = new MatTableDataSource<Mascota>(listMascotas);
+  loading: boolean = false;
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
+
+  constructor(private _snackBar: MatSnackBar) {}
 
   ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
@@ -34,6 +38,18 @@ export class ListadoMascotaComponent implements AfterViewInit {
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  eliminarMascota() {
+    this.loading = true;
+
+    setTimeout(() => {
+      this.loading = false;
+      this._snackBar.open('La Mascota fue eliminada con éxito', '', {
+        duration: 4000,
+        horizontalPosition: 'right',
+      });
+    }, 3000);    
   }
 
 }
