43834a907f4989646f8e787e3c81ad1d86f465ea
Joaquin Acosta <joaquinnof14@gmail.com>
2024-01-17 15:34:18 -0300

    integrado componente ver-mascota a backend

--- src/app/components/ver-mascota/ver-mascota.component.ts
+++ src/app/components/ver-mascota/ver-mascota.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Mascota } from 'src/app/interfaces/mascota';
+import { MascotaService } from 'src/app/services/mascota.service';
 
 @Component({
   selector: 'app-ver-mascota',
   templateUrl: './ver-mascota.component.html',
   styleUrls: ['./ver-mascota.component.css']
 })
-export class VerMascotaComponent {
+export class VerMascotaComponent implements OnInit {
+  id: number;
+  mascota!: Mascota;
+  loading: boolean = false;
+
+  /* mascota$!: Observable<Mascota>;  -- PIPE ASYNC */
+
+  constructor(
+    private _mascotaService: MascotaService, 
+    private aRoute: ActivatedRoute) {
+      this.id = Number(this.aRoute.snapshot.paramMap.get('id'));
+  }
+
+  ngOnInit(): void {
+    /* this.mascota$ = this._mascotaService.getMascota(this.id);  -- PIPE ASYNC */
+    this.obtenerMascota();
+  }
+
+  obtenerMascota() {
+    this.loading = true;
+    this._mascotaService.getMascota(this.id).subscribe(data => {
+      this.mascota = data;
+      this.loading = false;
+    })
+  }
 
 }
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,1 +1,4 @@
-export const environment = {};
+export const environment = {
+    endpoint: 'https://localhost:7211/'
+};
+
